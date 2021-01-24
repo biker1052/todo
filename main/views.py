1e1f8a9db453228fd52eaf4613ad9ae3df5133c2
biker1052 <uirchenkoden0303@rambler.ru>
2021-01-25 01:02:11 +0600

    Add books.html and booksDetail.html to project

--- main/views.py
+++ main/views.py
@@ -15,14 +15,32 @@ def second(request):
 def third(request):
     return HttpResponse("This page test3")    
 def books(request):
-     book_list = Book.objects.all()
-     return render(request,"books.html",{"book_list":book_list})  
+    book_list = Book.objects.all()
+    return render(request,"books.html",{"book_list":book_list})  
+def openbook(request,id):
+     book = Book.objects.get(id=id)
+     return render(request,"books_detail.html",{"book_obj":book})       
 def add_todo(request):
     form = request.POST
     text= form["todo_text"]
     todo = ToDo(text=text)
     todo.save()
     return redirect(test)  
+
+def add_book(request):
+    form = request.POST
+    book = Book(
+        title=form["title"],
+        subtitle=form["subtitle"],
+        description=form["description"],
+        price=form["price"],
+        genre=form["genre"],
+        author=form["author"],
+        year=form["year"],
+    )
+    book.save()
+    return redirect(books)  
+
 def delete_todo(request,id):
     todo = ToDo.objects.get(id=id)
     todo.delete()
