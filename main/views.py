cf443c3ebe9ceeed3b5cc6ac0cf8a2cdd52f4de5
biker1052 <uirchenkoden0303@rambler.ru>
2021-01-25 02:20:45 +0600

    Add todo closing

--- main/views.py
+++ main/views.py
@@ -64,6 +64,11 @@ def unmark_todo(request,id):
     todo = ToDo.objects.get(id=id)
     todo.is_favorite=False
     todo.save()
-    return redirect(test)                
+    return redirect(test)   
+def close_todo(request,id):
+    todo = ToDo.objects.get(id=id)
+    todo.is_closed=not todo.is_closed
+    todo.save()
+    return redirect(test)                    
     
         
